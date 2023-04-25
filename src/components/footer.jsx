65c5e7aeccd52de8984130690b8b063a6e621e98
Jim Barnett <hlbradow@yahoo.com>
2023-04-25 19:41:22 -0400

    first commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,7 @@
+import "./footer.css";
+
+function Footer() {
+    return <div className="footer">Footer here</div>;
+}
+
+export default Footer;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,7 @@
+import "./navbar.css";
+
+function Navbar() {
+    return <div className="navbar">menu here</div>;
+}
+
+export default Navbar;
