efd031f205f1245b9e47201939674f508860a180
Jim Barnett <hlbradow@yahoo.com>
2023-04-25 21:33:08 -0400

    Commit 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 
 function App() {
     return (
         <div className="App">
             <Navbar></Navbar>
-
-            <h1>The Polls</h1>
+            <br></br>
+            <h1>The Polling Station</h1>
 
             <Footer></Footer>
         </div>
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,12 @@
 import "./footer.css";
 
 function Footer() {
-    return <div className="footer">Footer here</div>;
+    return (
+        <div className="footer">
+            <h4>Connect With Us</h4>
+            <p>&copy; 2023 The Polling Station. All Rights Reserved.</p>
+        </div>
+    );
 }
 
 export default Footer;
